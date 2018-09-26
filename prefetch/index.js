318cfb4a4766e18a1678979b1fa41b16a46fcc69
Andrew Boktor <aboktor@microsoft.com>
2018-09-26 10:37:57 -0700

    adding prefetch POC

---
+++ prefetch/index.js
@@ -0,0 +1,28 @@
+window.onload = main;
+
+function main() {
+  setInterval(print, 1000);
+  setInterval(prefetch, 2000);
+  //setInterval(script, 10000);
+}
+
+function print() {
+  var message = 'A';
+  //message = 'B';
+  console.log(message);
+}
+
+function prefetch() {
+  var link = document.createElement('link');
+  link.rel = 'prefetch';
+  link.href = 'index.js';
+  document.head.appendChild(link);
+  console.log('PREFETCH');
+}
+
+// function script() {
+//   var script = document.createElement('script');
+//   script.src = 'index.js';
+//   document.head.appendChild(script);
+//   console.log('SCRIPT');
+// }
