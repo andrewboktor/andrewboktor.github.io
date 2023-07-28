68104d8cddbf6ed2c87fbf8bd8b337bfea1fc1de
Andrew Boktor <aboktor@microsoft.com>
2023-07-28 13:10:10 -0700

    Addin tinyComponents benchmarks

---
+++ tinyComponents/VanillaJs.chunk.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkBabyComponents=self.webpackChunkBabyComponents||[]).push([[733],{701:(e,n,t)=>{function o(e){!function(e,n){const t=document.createElement("div");t.innerText="Hello World!",e.appendChild(t)}(e)}t.r(n),t.d(n,{default:()=>o})}}]);
